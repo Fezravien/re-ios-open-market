0d6cbab1d0afec1090b455f796f59ee997160bba
Fezravien <yjaewoong@naver.com>
2021-08-26 14:33:26 +0900

    ✨ [Feat] : Request 모델 구현 - 상품 등록

---
+++ OpenMarket/OpenMarket/Models/Request/ItemRegistration.swift
@@ -0,0 +1,19 @@
+//
+//  ItemRegistration.swift
+//  OpenMarket
+//
+//  Created by Fezravien on 2021/08/26.
+//
+
+import Foundation
+
+struct ItemRegistration {
+    let title: String
+    let descriptions: String
+    let price: UInt
+    let currency: String
+    let stock: UInt32
+    let discountedPrice: UInt?
+    let images: [Data]
+    let password: String
+}
