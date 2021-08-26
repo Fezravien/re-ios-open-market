51bc0c72e81d4bbb5d62e6dced326fb7d53ddf8a
Fezravien <yjaewoong@naver.com>
2021-08-26 14:32:16 +0900

    ✨ [Feat] : Response 모델 구현 - 상품 조회

---
+++ OpenMarket/OpenMarket/Models/Response/Item.swift
@@ -0,0 +1,27 @@
+//
+//  Item.swift
+//  OpenMarket
+//
+//  Created by Fezravien on 2021/08/26.
+//
+
+import Foundation
+
+struct Item: Decodable, Equatable {
+    let id: UInt32
+    let title: String
+    let descriptions: String
+    let price: UInt
+    let currency: String
+    let stock: UInt
+    let discountPrice: UInt?
+    let thumbnails: [String]
+    let images: [String]
+    let registrationDate: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case id, title, descriptions, price, currency, stock, thumbnails, images
+        case discountPrice = "discounted_price"
+        case registrationDate = "registration_date"
+    }
+}
