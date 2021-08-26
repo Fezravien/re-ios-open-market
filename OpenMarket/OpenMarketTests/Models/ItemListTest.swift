82594750c90308f0f58fc9268f4375b23f3b88a1
Fezravien <yjaewoong@naver.com>
2021-08-26 18:00:58 +0900

    ✅ [Test] : Item 모델 Mock를 활용한 디코딩 테스트 진행

---
+++ OpenMarket/OpenMarketTests/Models/ItemListTest.swift
@@ -0,0 +1,20 @@
+//
+//  ItemListTest.swift
+//  OpenMarketTests
+//
+//  Created by Fezravien on 2021/08/26.
+//
+
+import XCTest
+
+class ItemListTest: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+}
