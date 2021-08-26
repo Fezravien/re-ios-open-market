bb53bd3fdf0ed8c775a65f8741f69d689153d1d4
Fezravien <yjaewoong@naver.com>
2021-08-26 18:14:47 +0900

    ✅ [Test] : ItemList 모델 Mock 데이터를 활용한 디코딩 테스트 진행

--- OpenMarket/OpenMarketTests/Models/ItemListTest.swift
+++ OpenMarket/OpenMarketTests/Models/ItemListTest.swift
@@ -6,15 +6,49 @@
 //
 
 import XCTest
+@testable import OpenMarket
 
 class ItemListTest: XCTestCase {
-
+    var jsonDecoder: JSONDecoder?
+    var mockItemListAsset: Data?
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.jsonDecoder = JSONDecoder()
+        self.mockItemListAsset = NSDataAsset(name: "Items")?.data
     }
 
     override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        self.jsonDecoder = nil
+        self.mockItemListAsset = nil
     }
-
+    
+    func test_Model_Item_Decoding_성공() {
+        // given
+        let mockItemInput = ItemList(page: 1, items: [ItemList.Item(id: 1,
+                                                                   title: "MacBook Pro",
+                                                                   price: 1690,
+                                                                   currency: "USD",
+                                                                   stock: 0,
+                                                                   discountPrice: nil,
+                                                                   thumbnails: [
+                                                                    "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/thumbnails/1-1.png",
+                                                                    "https://camp-open-market.s3.ap-northeast-2.amazonaws.com/thumbnails/1-2.png"
+                                                                ],
+                                                                   registrationDate: 1611523563.7237701)])
+        
+        // when
+        guard let data = try? jsonDecoder!.decode(Item.self, from: mockItemListAsset!) else {
+            XCTAssertFalse(true)
+            return
+        }
+        
+        // then
+        XCTAssertEqual(data.id, mockItemInput.items.first!.id)
+        XCTAssertEqual(data.title, mockItemInput.items.first!.title)
+        XCTAssertEqual(data.price, mockItemInput.items.first!.price)
+        XCTAssertEqual(data.currency, mockItemInput.items.first!.currency)
+        XCTAssertEqual(data.stock, mockItemInput.items.first!.stock)
+        XCTAssertEqual(data.thumbnails, mockItemInput.items.first!.thumbnails)
+        XCTAssertEqual(data.registrationDate, mockItemInput.items.first!.registrationDate)
+    }
 }
