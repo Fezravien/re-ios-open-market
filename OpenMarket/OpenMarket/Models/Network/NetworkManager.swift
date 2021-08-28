ab10b428ef0ccf95beef5e7f7ed1c33ec173b769
Fezravien <yjaewoong@naver.com>
2021-08-28 13:46:17 +0900

    ✨ [Feat] : 네트워크 구조 구현

---
+++ OpenMarket/OpenMarket/Models/Network/NetworkManager.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkManager.swift
+//  OpenMarket
+//
+//  Created by Fezravien on 2021/08/26.
+//
+
+import Foundation
+
+final class NetworkManager {
+    private let loader: MarketNetwork
+    private let decoder: OpenMarketDecode
+    
+    init(loader: MarketNetwork, decoder: OpenMarketDecode) {
+        self.loader = loader
+        self.decoder = decoder
+    }
+    
+    func excuteDecode<T>(request: URLRequest, decodeType: T.Type, completion: @escaping (Result<T, Error>) -> Void) where T: Decodable {
+        self.loader.excuteNetwork(request: request) { [weak self] result in
+            switch result {
+            case .success(let data):
+                do {
+                    guard let jsonDecode = try self?.decoder.decode(T.self, from: data) else { return }
+                    completion(.success(jsonDecode))
+                } catch {
+                    completion(.failure(MarketModelError.decoding(error)))
+                }
+            case .failure(let error):
+                completion(.failure(MarketModelError.network(error)))
+            }
+        }
+    }
+}
---
+++ OpenMarket/OpenMarket/ViewModel/OpenMarketViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  OpenMarketViewModel.swift
+//  OpenMarket
+//
+//  Created by Fezravien on 2021/08/26.
+//
+
+import Foundation
