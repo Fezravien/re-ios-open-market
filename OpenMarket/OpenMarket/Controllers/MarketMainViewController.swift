1578b7eb16e0d8d4e7420830a28b8c8470aef9ff
Fezravien <yjaewoong@naver.com>
2021-08-30 23:05:53 +0900

    ✨ [Feat] : 메인 화면 - List 기능 구현
    - MarketListCollectionViewCell 구현
    - indicater 구현
    네트워크 Fetch 전 까지 동작
    - 네트워크 작업 (Fetch) 를 통해 ItemList를 받아 Collection에 뿌림

--- OpenMarket/OpenMarket/Controllers/MarketMainViewController.swift
+++ OpenMarket/OpenMarket/Controllers/MarketMainViewController.swift
@@ -14,6 +14,12 @@ final class MarketMainViewController: UIViewController {
         case grid = "Grid"
     }
     
+    private let marketindicater: UIActivityIndicatorView = {
+        let indicater = UIActivityIndicatorView(style: .large)
+        indicater.color = .darkGray
+        indicater.translatesAutoresizingMaskIntoConstraints = false
+        return indicater
+    }()
     private let marketSegmentController: UISegmentedControl = {
         let segmentController = UISegmentedControl(items: segmentStyle.allCases.map { $0.rawValue })
         segmentController.selectedSegmentIndex = 0
@@ -23,16 +29,39 @@ final class MarketMainViewController: UIViewController {
     }()
     private let marketCollectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        collectionView.register(MarketListCollectionViewCell.self, forCellWithReuseIdentifier: MarketListCollectionViewCell.identifier)
         collectionView.showsVerticalScrollIndicator = false
         collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.backgroundColor = .darkGray
+        collectionView.backgroundColor = .white
         return collectionView
     }()
+    private let marketViewModel = MarketViewModel()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bindData()
+        fetchMarketData()
         setSegment()
         setConstraint()
+        setDelegate()
+        setDataSource()
+        
+    }
+    
+    private func bindData() {
+        self.marketViewModel.observer = {
+            DispatchQueue.main.async {
+                self.marketCollectionView.reloadData()
+            }
+        }
+    }
+    
+    private func setDataSource() {
+        self.marketCollectionView.dataSource = self
+    }
+    
+    private func setDelegate() {
+        self.marketCollectionView.delegate = self
     }
     
     private func setSegment() {
@@ -44,6 +73,7 @@ final class MarketMainViewController: UIViewController {
     }
     
     private func setConstraint() {
+        setMarketIndicaterConstraint()
         setMarketCollectionViewConstraint()
     }
     
@@ -57,4 +87,40 @@ final class MarketMainViewController: UIViewController {
             self.marketCollectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
         ])
     }
+    
+    private func setMarketIndicaterConstraint() {
+        self.marketCollectionView.addSubview(self.marketindicater)
+        
+        NSLayoutConstraint.activate([
+            self.marketindicater.centerXAnchor.constraint(equalTo: self.marketCollectionView.centerXAnchor),
+            self.marketindicater.centerYAnchor.constraint(equalTo: self.marketCollectionView.centerYAnchor),
+        ])
+    }
+    
+    private func fetchMarketData() {
+        self.marketindicater.startAnimating()
+        guard let fetchURL = NetworkConstant.itemList(page: 1).url else { return }
+        let request = URLRequest(url: fetchURL)
+        self.marketViewModel.fetch(request: request, decodeType: ItemList.self) { [weak self] result in
+            DispatchQueue.main.async {
+                self?.marketindicater.stopAnimating()
+            }
+        }
+    }
+}
+
+extension MarketMainViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.marketViewModel.marketItemsCount
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MarketListCollectionViewCell.identifier, for: indexPath) as? MarketListCollectionViewCell else { return UICollectionViewCell() }
+        cell.listCellConfiguration(data: marketViewModel.marketItem(index: indexPath.row))
+        return cell
+    }
+}
+
+extension MarketMainViewController: UICollectionViewDelegate {
+    
 }
---
+++ OpenMarket/OpenMarket/Views/MarketListCollectionViewCell.swift
@@ -0,0 +1,117 @@
+//
+//  MarketListCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by Fezravien on 2021/08/28.
+//
+
+import UIKit
+
+class MarketListCollectionViewCell: UICollectionViewCell {
+    static let identifier = "MarketListCell"
+    
+    private let itemImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    private let itemTitle: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .headline)
+        label.numberOfLines = 2
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let itemPrice: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .footnote)
+        label.textColor = .systemGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let itemStock: UILabel = {
+        let label = UILabel()
+        label.font = UIFont.preferredFont(forTextStyle: .caption2)
+        label.textColor = .systemGray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    
+    func listCellConfiguration(data: Item) {
+        setConstraints()
+        self.itemImageView.image = UIImage(data: downloadImage(data.thumbnails.first!))
+        self.itemTitle.text = data.title
+        self.itemStock.text = convertStockFormat(stock: data.stock)
+        self.itemPrice.text = convertPriceFormat(currency: data.currency, price: data.price)
+    }
+    
+    private func downloadImage(_ imageURL: String) -> Data {
+        guard let url = URL(string: imageURL),
+              let image = try? Data(contentsOf: url) else {
+            
+            // TODO: - 오류처리를 통해 이미지가 없다고 판단하도록 할 수 있을듯
+            return Data()
+        }
+        
+        return image
+    }
+    
+    private func convertStockFormat(stock: UInt) -> String {
+        return "잔여수량 : \(stock)"
+    }
+    
+    private func convertPriceFormat(currency: String, price: UInt) -> String {
+        return "\(currency) \(price)"
+    }
+    
+    private func setConstraints() {
+        setItemImageViewConstraint()
+        setItemTitleConstraint()
+        setItemStockConstraint()
+        setItemPriceConstraint()
+    }
+    
+    private func setItemImageViewConstraint() {
+        self.contentView.addSubview(self.itemImageView)
+        
+        NSLayoutConstraint.activate([
+            self.itemImageView.leadingAnchor.constraint(equalTo: self.contentView.leadingAnchor, constant: 10),
+            self.itemImageView.topAnchor.constraint(equalTo: self.contentView.topAnchor, constant: 10),
+            self.itemImageView.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor, constant: -10),
+            self.itemImageView.widthAnchor.constraint(equalTo: self.contentView.widthAnchor, multiplier: 3/10),
+            self.itemImageView.heightAnchor.constraint(equalTo: self.itemImageView.widthAnchor),
+        ])
+    }
+    
+    private func setItemTitleConstraint() {
+        self.contentView.addSubview(self.itemTitle)
+        
+        NSLayoutConstraint.activate([
+            self.itemTitle.leadingAnchor.constraint(equalTo: self.itemImageView.trailingAnchor, constant: 25),
+            self.itemTitle.topAnchor.constraint(equalTo: self.itemImageView.topAnchor, constant: 15),
+            self.itemTitle.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -10)
+        ])
+    }
+    
+    private func setItemStockConstraint() {
+        self.contentView.addSubview(self.itemStock)
+        
+        NSLayoutConstraint.activate([
+            self.itemStock.leadingAnchor.constraint(equalTo: self.itemTitle.leadingAnchor),
+            self.itemStock.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -10),
+            self.itemStock.bottomAnchor.constraint(equalTo: self.itemImageView.bottomAnchor, constant: -30)
+        ])
+    }
+    
+    private func setItemPriceConstraint() {
+        self.contentView.addSubview(self.itemPrice)
+        
+        NSLayoutConstraint.activate([
+            self.itemPrice.leadingAnchor.constraint(equalTo: self.itemTitle.leadingAnchor),
+            self.itemPrice.bottomAnchor.constraint(equalTo: self.itemStock.topAnchor, constant: -10),
+            self.itemPrice.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor, constant: -10),
+        ])
+    }
+}
