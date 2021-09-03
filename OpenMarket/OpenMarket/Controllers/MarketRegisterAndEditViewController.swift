16bf57d83bca03a7437f9b0c8ac1e37d59b8b2b8
Fezravien <yjaewoong@naver.com>
2021-09-03 11:16:07 +0900

    ✨ [Feat] : 상품 등록/수정 페이지의 이미지 보여주기 위한 기능

---
+++ OpenMarket/OpenMarket/Controllers/MarketRegisterAndEditViewController.swift
@@ -0,0 +1,129 @@
+//
+//  MarketRegisterAndEditViewController.swift
+//  OpenMarket
+//
+//  Created by Fezravien on 2021/09/02.
+//
+
+import UIKit
+import PhotosUI
+
+final class MarketRegisterAndEditViewController: UIViewController {
+    
+    private enum State {
+        static let registration = "상품등록"
+        static let edit = "상품수정"
+    }
+    
+    private let imageCollectionView: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.register(MarketImageCollectionViewCell.self, forCellWithReuseIdentifier: MarketImageCollectionViewCell.identifier)
+        collectionView.register(MarketImageCollectionViewHeader.self, forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: MarketImageCollectionViewHeader.identifier)
+        collectionView.backgroundColor = .white
+        collectionView.showsHorizontalScrollIndicator = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    private let imageAddButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .systemOrange
+        button.setImage(.add, for: .normal)
+        button.tintColor = .white
+        button.layer.cornerRadius = 10
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    private let itemPickerView: PHPickerViewController = {
+        var configuration = PHPickerConfiguration()
+        configuration.selectionLimit = 5
+        configuration.filter = .any(of: [.images, .livePhotos])
+        let pickerView = PHPickerViewController(configuration: configuration)
+        return pickerView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        setDataSource()
+        setDelegate()
+        setConstraint()
+    }
+    
+    private func setDataSource() {
+        self.imageCollectionView.dataSource = self
+    }
+    
+    private func setDelegate() {
+        self.imageCollectionView.delegate = self
+        self.itemPickerView.delegate = self
+    }
+    
+    private func setConstraint() {
+        setImageCollectionView()
+        setImageAddButtonConstraint()
+    }
+    
+    private func setImageCollectionView() {
+        self.view.addSubview(self.imageCollectionView)
+        
+        NSLayoutConstraint.activate([
+            self.imageCollectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 15),
+            self.imageCollectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            self.imageCollectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -15),
+            self.imageCollectionView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 1/10)
+        ])
+    }
+    
+    private func setImageAddButtonConstraint() {
+        
+    }
+}
+
+extension MarketRegisterAndEditViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 15
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = self.imageCollectionView.dequeueReusableCell(withReuseIdentifier: MarketImageCollectionViewCell.identifier, for: indexPath) as? MarketImageCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        cell.configurateImageCell()
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        guard let reuseableView = self.imageCollectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: MarketImageCollectionViewHeader.identifier, for: indexPath) as? MarketImageCollectionViewHeader else {
+            return UICollectionReusableView()
+        }
+        reuseableView.configrateHeader()
+        return reuseableView
+    }
+}
+
+extension MarketRegisterAndEditViewController: UICollectionViewDelegate {
+    
+}
+
+extension MarketRegisterAndEditViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return CGSize(width: self.view.frame.height * 1/13, height: self.view.frame.height * 1/13)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: self.view.frame.height * 1/13, height: self.view.frame.height * 1/50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 0)
+    }
+}
+
+extension MarketRegisterAndEditViewController: PHPickerViewControllerDelegate {
+    func picker(_ picker: PHPickerViewController, didFinishPicking results: [PHPickerResult]) {
+
+    }
+}
---
+++ OpenMarket/OpenMarket/Views/RegisterAndEdit/MarketImageCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MarketImageCollectionViewCell.swift
+//  OpenMarket
+//
+//  Created by Fezravien on 2021/09/02.
+//
+
+import UIKit
+
+final class MarketImageCollectionViewCell: UICollectionViewCell {
+    static let identifier = "MarketImageCell"
+    
+    func configurateImageCell() {
+        self.contentView.backgroundColor = .purple
+    }
+}
