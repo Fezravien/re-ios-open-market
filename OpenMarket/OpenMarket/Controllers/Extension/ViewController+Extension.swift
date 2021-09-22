a5e75abced951d860a39204bce4e0f487de0e606
Fezravien <yjaewoong@naver.com>
2021-09-22 18:09:46 +0900

    ♻️ [Refactor] : 단순 알림기능에 클로저를 추가함
    클로저를 추가함으로써 확인 버튼을 누른 후 이후에 처리할 로직을 넣을 수 있음
    ex) 수정완료 버튼 누른 후 상세페이지 새로고침 하기

--- OpenMarket/OpenMarket/Controllers/Extension/ViewController+Extension.swift
+++ OpenMarket/OpenMarket/Controllers/Extension/ViewController+Extension.swift
@@ -10,9 +10,11 @@ import UIKit
 extension UIViewController {
     typealias AlertActionHandler = ((UIAlertAction) -> Void)
     
-    func alert(title: String, message: String? = nil, okTitle: String = "OK") {
+    func alert(title: String, message: String? = nil, okTitle: String = "OK", completion: (() -> ())? = nil ) {
         let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let okAction = UIAlertAction(title: okTitle, style: .cancel)
+        let okAction = UIAlertAction(title: okTitle, style: .cancel) { _ in
+            completion?()
+        }
         alert.addAction(okAction)
     
         self.present(alert, animated: true)
