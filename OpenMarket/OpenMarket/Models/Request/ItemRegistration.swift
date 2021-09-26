5515cd37a7fc4ae5c81ce6c476ce217bc56136df
Fezravien <yjaewoong@naver.com>
2021-09-26 16:36:52 +0900

    🐛 [Fix] : Multi-Part/Form-Data 생성을 위한 모델 수정
    서버의 변수를 고려하여 프로토콜로써 메소드 정의

--- OpenMarket/OpenMarket/Models/Request/ItemRegistration.swift
+++ OpenMarket/OpenMarket/Models/Request/ItemRegistration.swift
@@ -12,8 +12,21 @@ struct ItemRegistration: MultiPartForm {
     let descriptions: String
     let price: UInt
     let currency: String
-    let stock: UInt32
+    let stock: UInt
     let discountedPrice: UInt?
     let images: [Data]
     let password: String
+    
+    var asDictionary: [String : Any?] {
+        [
+            "title": self.title,
+            "descriptions": self.descriptions,
+            "price": self.price,
+            "currency": self.currency,
+            "stock": self.stock,
+            "discounted_price": self.discountedPrice,
+            "images": self.images,
+            "password": self.password
+        ]
+    }
 }
