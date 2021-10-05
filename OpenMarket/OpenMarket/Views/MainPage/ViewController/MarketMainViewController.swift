ced39d7aa4bd9e009f00a8f3bb9cc06d6feafa35
Fezravien <yjaewoong@naver.com>
2021-10-05 19:17:55 +0900

    🚚 [Style] : Mark 주석으로 가독성 향상 및 코드 정리

--- OpenMarket/OpenMarket/Views/MainPage/ViewController/MarketMainViewController.swift
+++ OpenMarket/OpenMarket/Views/MainPage/ViewController/MarketMainViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 final class MarketMainViewController: UIViewController, RegisterationToMainDelegate, DetailToMainDelegate {
+    
+    // MARK: - Name Space
+    
     private enum segmentStyle: String, CaseIterable {
         case list = "List"
         case grid = "Grid"
@@ -18,21 +21,23 @@ final class MarketMainViewController: UIViewController, RegisterationToMainDeleg
         case delete
     }
     
-    private let marketindicater: UIActivityIndicatorView = {
+    // MARK: - variable, constant and UI Initialization
+    
+    private let itemListLoadingindicater: UIActivityIndicatorView = {
         let indicater = UIActivityIndicatorView(style: .large)
         indicater.color = .darkGray
         indicater.hidesWhenStopped = true
         indicater.translatesAutoresizingMaskIntoConstraints = false
         return indicater
     }()
-    private let marketSegmentController: UISegmentedControl = {
-        let segmentController = UISegmentedControl(items: segmentStyle.allCases.map { $0.rawValue })
-        segmentController.selectedSegmentIndex = 0
-        segmentController.tintColor = .white
-        segmentController.backgroundColor = .systemOrange
-        return segmentController
+    private let itemListModeSegmentControl: UISegmentedControl = {
+        let segmentControl = UISegmentedControl(items: segmentStyle.allCases.map { $0.rawValue })
+        segmentControl.selectedSegmentIndex = 0
+        segmentControl.tintColor = .white
+        segmentControl.backgroundColor = .systemOrange
+        return segmentControl
     }()
-    private let marketCollectionView: UICollectionView = {
+    private let itemListCollectionView: UICollectionView = {
         let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
         collectionView.register(MarketListCollectionViewCell.self, forCellWithReuseIdentifier: MarketListCollectionViewCell.identifier)
         collectionView.register(MarketGridCollectionViewCell.self, forCellWithReuseIdentifier: MarketGridCollectionViewCell.identifier)
@@ -41,10 +46,12 @@ final class MarketMainViewController: UIViewController, RegisterationToMainDeleg
         collectionView.backgroundColor = .white
         return collectionView
     }()
-    private let marketViewModel = MarketMainViewModel()
+    private let marketMainViewModel = MarketMainViewModel()
     private var marketDetailViewController: MarketDetailViewController?
     private var page: UInt = 1
     private var mode: Mode = .normal
+    
+    // MARK: - View life cycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -57,53 +64,40 @@ final class MarketMainViewController: UIViewController, RegisterationToMainDeleg
         setDataSource()
     }
     
+    // MARK: - Data binding with ViewModel (MainViewModel)
+    
     private func bindData() {
-        self.marketViewModel.observer = {
-            if self.marketViewModel.getMarketItemsCount == 0 {
+        self.marketMainViewModel.observer = {
+            if self.marketMainViewModel.getMarketItemsCount == 0 {
                 self.page = 1
                 self.fetchMarketData()
                 return
             }
             
             DispatchQueue.main.async {
-                self.marketindicater.startAnimating()
-                self.marketCollectionView.reloadData()
+                self.itemListLoadingindicater.startAnimating()
+                self.itemListCollectionView.reloadData()
                 if self.mode == .delete {
                     self.mode = .normal
-                    self.marketCollectionView.setContentOffset(CGPoint(x: 0, y: self.marketCollectionView.contentInset.top), animated: true)
+                    self.itemListCollectionView.setContentOffset(CGPoint(x: 0, y: self.itemListCollectionView.contentInset.top), animated: true)
                 }
-                self.marketindicater.stopAnimating()
+                self.itemListLoadingindicater.stopAnimating()
             }
         }
     }
     
+    // MARK: - Set dataSource and delegate at self
+    
     private func setDataSource() {
-        self.marketCollectionView.dataSource = self
-        self.marketCollectionView.prefetchDataSource = self
+        self.itemListCollectionView.dataSource = self
+        self.itemListCollectionView.prefetchDataSource = self
     }
     
     private func setDelegate() {
-        self.marketCollectionView.delegate = self
+        self.itemListCollectionView.delegate = self
     }
     
-    private func setSegment() {
-        for index in 0..<segmentStyle.allCases.count {
-            self.marketSegmentController.setWidth(self.view.frame.width * 1/5, forSegmentAt: index)
-        }
-        self.marketSegmentController.addTarget(self, action: #selector(changedSegmentController), for: .valueChanged)
-        self.navigationItem.titleView = self.marketSegmentController
-    }
-    
-    @objc private func changedSegmentController() {
-        switch self.marketSegmentController.selectedSegmentIndex {
-        case 0:
-            self.marketCollectionView.reloadData()
-        case 1:
-            self.marketCollectionView.reloadData()
-        default:
-            return
-        }
-    }
+    // MARK: - Set UI configuration
     
     private func setNavigationItem() {
         self.navigationController?.view.backgroundColor = .systemGray3
@@ -117,46 +111,71 @@ final class MarketMainViewController: UIViewController, RegisterationToMainDeleg
         self.navigationController?.pushViewController(marketRegisterAndEditViewController, animated: true)
     }
     
+    private func setSegment() {
+        for index in 0..<segmentStyle.allCases.count {
+            self.itemListModeSegmentControl.setWidth(self.view.frame.width * 1/5, forSegmentAt: index)
+        }
+        self.itemListModeSegmentControl.addTarget(self, action: #selector(changedSegmentController), for: .valueChanged)
+        self.navigationItem.titleView = self.itemListModeSegmentControl
+    }
+    
+    @objc private func changedSegmentController() {
+        switch self.itemListModeSegmentControl.selectedSegmentIndex {
+        case 0:
+            self.itemListCollectionView.reloadData()
+        case 1:
+            self.itemListCollectionView.reloadData()
+        default:
+            return
+        }
+    }
+    
+    // MARK: - Set constraint UI
+    
     private func setConstraint() {
         setMarketIndicaterConstraint()
         setMarketCollectionViewConstraint()
     }
     
     private func setMarketCollectionViewConstraint() {
-        self.view.addSubview(self.marketCollectionView)
+        self.view.addSubview(self.itemListCollectionView)
         
         NSLayoutConstraint.activate([
-            self.marketCollectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            self.marketCollectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
-            self.marketCollectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
-            self.marketCollectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            self.itemListCollectionView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            self.itemListCollectionView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            self.itemListCollectionView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            self.itemListCollectionView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
         ])
     }
     
     private func setMarketIndicaterConstraint() {
-        self.marketCollectionView.addSubview(self.marketindicater)
+        self.itemListCollectionView.addSubview(self.itemListLoadingindicater)
         
         NSLayoutConstraint.activate([
-            self.marketindicater.centerXAnchor.constraint(equalTo: self.marketCollectionView.centerXAnchor),
-            self.marketindicater.centerYAnchor.constraint(equalTo: self.marketCollectionView.centerYAnchor),
+            self.itemListLoadingindicater.centerXAnchor.constraint(equalTo: self.itemListCollectionView.centerXAnchor),
+            self.itemListLoadingindicater.centerYAnchor.constraint(equalTo: self.itemListCollectionView.centerYAnchor),
         ])
     }
     
-    private func fetchMarketData(page: UInt = 1) {
-        self.marketindicater.startAnimating()
-        guard let request = self.marketViewModel.createRequest(page) else { return }
+    // MARK: - Fetch itmeList data from Server
     
-        self.marketViewModel.fetch(request: request, decodeType: ItemList.self) { [weak self] _ in
+    private func fetchMarketData(page: UInt = 1) {
+        self.itemListLoadingindicater.startAnimating()
+        guard let request = self.marketMainViewModel.createRequest(page) else { return }
+    
+        self.marketMainViewModel.fetch(request: request, decodeType: ItemList.self) { [weak self] _ in
             // TODO: - 실패 오류를 받아서 Alert로 사용자에게 알릴 수 있다.
             DispatchQueue.main.async {
-                self?.marketindicater.stopAnimating()
+                self?.itemListLoadingindicater.stopAnimating()
             }
         }
     }
     
+    // MARK: - Get notifications from other objects. (Delegate Pattern)
+    
     func refreshMainItemList() {
         self.mode = .delete
-        self.marketViewModel.removeAllItems()
+        self.marketMainViewModel.removeAllItems()
     }
     
     func displayRegisteratedItem(item: Item) {
@@ -168,29 +187,31 @@ final class MarketMainViewController: UIViewController, RegisterationToMainDeleg
     }
 }
 
+// MARK: - UICollectionView - DataSource
+
 extension MarketMainViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return self.marketViewModel.getMarketItemsCount
+        return self.marketMainViewModel.getMarketItemsCount
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        switch self.marketSegmentController.selectedSegmentIndex {
+        switch self.itemListModeSegmentControl.selectedSegmentIndex {
         case 0:
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MarketListCollectionViewCell.identifier, for: indexPath) as? MarketListCollectionViewCell else { return UICollectionViewCell() }
-            let item = self.marketViewModel.getMarketItem(index: indexPath.row)
-            self.marketViewModel.downloadImage(item.thumbnails.first ?? "") { imageData in
+            let item = self.marketMainViewModel.getMarketItem(index: indexPath.row)
+            self.marketMainViewModel.downloadImage(item.thumbnails.first ?? "") { imageData in
                 cell.configurateListCellImage(imageData: imageData)
             }
-            cell.configurateListCellText(data: self.marketViewModel.getMarketItem(index: indexPath.row))
+            cell.configurateListCellText(data: self.marketMainViewModel.getMarketItem(index: indexPath.row))
             return cell
         case 1:
             guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: MarketGridCollectionViewCell.identifier, for: indexPath) as? MarketGridCollectionViewCell else { return UICollectionViewCell() }
-            let item = self.marketViewModel.getMarketItem(index: indexPath.row)
-            self.marketViewModel.downloadImage(item.thumbnails.first ?? "") { imageData in
+            let item = self.marketMainViewModel.getMarketItem(index: indexPath.row)
+            self.marketMainViewModel.downloadImage(item.thumbnails.first ?? "") { imageData in
                 cell.configurateGridCellImage(imageData: imageData)
             }
-            cell.configurateGridCellText(data: self.marketViewModel.getMarketItem(index: indexPath.row))
+            cell.configurateGridCellText(data: self.marketMainViewModel.getMarketItem(index: indexPath.row))
             return cell
         default:
             return UICollectionViewCell()
@@ -198,18 +219,22 @@ extension MarketMainViewController: UICollectionViewDataSource {
     }
 }
 
+// MARK: - UICollectionView - Delegate
+
 extension MarketMainViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         self.marketDetailViewController = MarketDetailViewController()
         self.marketDetailViewController?.detailToMainDelegate = self
-        self.marketDetailViewController?.setDetailViewController(item: self.marketViewModel.getMarketItem(index: indexPath.row))
+        self.marketDetailViewController?.setDetailViewController(item: self.marketMainViewModel.getMarketItem(index: indexPath.row))
         self.navigationController?.pushViewController(self.marketDetailViewController ?? MarketDetailViewController(), animated: true)
     }
 }
 
+// MARK: - UICollectionView - FlowLayout
+
 extension MarketMainViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        switch self.marketSegmentController.selectedSegmentIndex {
+        switch self.itemListModeSegmentControl.selectedSegmentIndex {
         case 0:
             return CGSize(width: self.view.frame.width, height: self.view.frame.height * 1/6)
         case 1:
@@ -220,7 +245,7 @@ extension MarketMainViewController: UICollectionViewDelegateFlowLayout {
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
-        if self.marketSegmentController.selectedSegmentIndex == 1 {
+        if self.itemListModeSegmentControl.selectedSegmentIndex == 1 {
             return UIEdgeInsets(top: 10, left: 10, bottom: 10, right: 10)
         } else {
             return UIEdgeInsets()
@@ -228,10 +253,12 @@ extension MarketMainViewController: UICollectionViewDelegateFlowLayout {
     }
 }
 
+// MARK: - UICollecionView - Prefetching (Paging)
+
 extension MarketMainViewController: UICollectionViewDataSourcePrefetching {
     func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
         for indexPath in indexPaths {
-            if marketViewModel.getMarketItemsCount == indexPath.row + 2 {
+            if marketMainViewModel.getMarketItemsCount == indexPath.row + 2 {
                 self.page += 1
                 fetchMarketData(page: page)
             }
